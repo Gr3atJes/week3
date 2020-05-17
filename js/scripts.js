4b4033a987290d7e6a2b4580489e6604cce3c50b
Gr3atJes <mathiujes@gmail.com>
2020-05-17 20:22:00 +0300

    what we do layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  $("#development-image").click(function(){
-    $("#development-image").slideDown('1500').hide('1000');
-    $("#development").show('1500');
-  });
+ $("img").click(function() {
+   $("p").toggle();
+ });
+});
   $("#development").click(function(){
     $("#development").slideUp('1500');
     $("#development-image").slideDown('1500');
