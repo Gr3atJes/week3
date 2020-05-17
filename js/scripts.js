7fcdb139f0a9173e119911f7aee95d9bc1aa2242
Gr3atJes <mathiujes@gmail.com>
2020-05-17 19:51:46 +0300

    new file change

--- js/scripts.js
+++ js/scripts.js
@@ -1,112 +1,32 @@
-//ui logic
 $(document).ready(function(){
-  //WHAT WE DO
-  $("div#design-show").click(function(){
-    $(this).slideUp(600);
-    $("div#design-hide").slideDown(600);
+  $("#development-image").click(function(){
+    $("#development-image").slideDown('1500').hide('1000');
+    $("#development").show('1500');
   });
-
-  $("div#design-hide").click(function(){
-    $(this).slideUp(600);
-    $("div#design-show").slideDown(600);
+  $("#development").click(function(){
+    $("#development").slideUp('1500');
+    $("#development-image").slideDown('1500');
   });
-
-  $("div#development-show").click(function(){
-    $(this).slideUp(600);
-    $("div#development-hide").slideDown(600);
-  });
-
-  $("div#development-hide").click(function(){
-    $(this).slideUp(600);
-    $("div#development-show").slideDown(600);
-  });
-
-  $("div#product-show").click(function(){
-    $(this).slideUp(600);
-    $("div#product-hide").slideDown(600);
-  });
-
-  $("div#product-hide").click(function(){
-    $(this).slideUp(600);
-    $("div#product-show").slideDown(600);
-  });
-
-  //PROJECTS
-  $("#work1-show").mouseenter(function(){
-    $("#work1-hide").parent().children("#work1-hide").fadeIn();
-  }).mouseleave(function(){
-    $("#work1-hide").parent().children("#work1-hide").fadeOut();
-  });
-
-  $("#work2-show").mouseenter(function(){
-    $("#work2-hide").parent().children("#work2-hide").fadeIn();
-  }).mouseleave(function(){
-    $("#work2-hide").parent().children("#work2-hide").fadeOut();
-  });
-
-  $("#work3-show").mouseenter(function(){
-    $("#work3-hide").parent().children("#work3-hide").fadeIn();
-  }).mouseleave(function(){
-    $("#work3-hide").parent().children("#work3-hide").fadeOut();
-  });
-
-  $("#work4-show").mouseenter(function(){
-    $("#work4-hide").parent().children("#work4-hide").fadeIn();
-  }).mouseleave(function(){
-    $("#work4-hide").parent().children("#work4-hide").fadeOut();
-  });
-
-  $("#work5-show").mouseenter(function(){
-    $("#work5-hide").parent().children("#work5-hide").fadeIn();
-  }).mouseleave(function(){
-    $("#work5-hide").parent().children("#work5-hide").fadeOut();
-  });
-
-  $("#work6-show").mouseenter(function(){
-    $("#work6-hide").parent().children("#work6-hide").fadeIn();
-  }).mouseleave(function(){
-    $("#work6-hide").parent().children("#work6-hide").fadeOut();
-  });
-
-  $("#work7-show").mouseenter(function(){
-    $("#work7-hide").parent().children("#work7-hide").fadeIn();
-  }).mouseleave(function(){
-    $("#work7-hide").parent().children("#work7-hide").fadeOut();
-  });
-
-  $("#work8-show").mouseenter(function(){
-    $("#work8-hide").parent().children("#work8-hide").fadeIn();
-  }).mouseleave(function(){
-    $("#work8-hide").parent().children("#work8-hide").fadeOut();
-  });
-
-  $("#sth").click(function(){
-    $("body").fadeOut(8000);
-    alert("thanos effect");
-  });
-
 });
 
-
-//business logic
 $(document).ready(function(){
-  //FORM EVENT//
-  $("#message").keypress(function(){
-    $("#hidden").fadeIn(2000);
+  $("#design-image").click(function(){
+    $("#design-image").slideDown('1500').hide('1000');
+    $("#design").show('1500');
   });
-
-  $("#my-form").submit(function(event){
-    let name = $("#name").val();
-    let email = $("#email").val();
-    let message = $("#message").val();
-
-    $("#user").text(name);
-    $(".alert").slideDown();
-    $("#close-alert").click(function(){
-      $(".alert").slideUp();
-      $("#my-form").trigger("reset");
-    });
-    event.preventDefault();
+  $("#design").click(function(){
+    $("#design").slideUp('1500');
+    $("#design-image").slideDown('1500');
   });
+});
 
-})
+$(document).ready(function(){
+  $("#product-image").click(function(){
+    $("#product-image").slideDown('1500').hide('1000');
+    $("#product").show('1500');
+  });
+  $("#product").click(function(){
+    $("#product").slideUp('1500');
+    $("#product-image").slideDown('1500');
+  });
+});
