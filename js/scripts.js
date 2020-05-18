daafbe715d4fe9a141b54f9fff52945f8a9841cb
Gr3atJes <mathiujes@gmail.com>
2020-05-18 11:13:18 +0300

    portfolio update

--- js/scripts.js
+++ js/scripts.js
@@ -31,20 +31,21 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function(){
-  $("#work1").mouseover(function(){
-    $("#overlay").show();
-  }).mouseout(function(){
-    $("#overlay").hide();
-  });
+$("#work1-show").mouseenter(function(){
+  $("#work1-hide").parent().children("#work1-hide").fadeIn();
+}).mouseleave(function(){
+  $("#work1-hide").parent().children("#work1-hide").fadeOut();
 });
-$(document).ready(function(){
-  $("#work2").mouseover(function(){
-    $("#overlay2").show();
-  }).mouseout(function(){
-    $("#overlay2").hide();
-  });
+
+$("#work2-show").mouseenter(function(){
+  $("#work2-hide").parent().children("#work2-hide").fadeIn();
+}).mouseleave(function(){
+  $("#work2-hide").parent().children("#work2-hide").fadeOut();
 });
+
+
+
+
 $(document).ready(function(){
   $("#work3").mouseover(function(){
     $("#overlay3").show();
