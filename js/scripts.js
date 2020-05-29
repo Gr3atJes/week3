683012181c94deb837f8c81106fbdc8decd96e62
Gr3atJes <mathiujes@gmail.com>
2020-05-29 06:48:10 +0300

    fixing mailchimp form to link with email account, portfolio section hover effect change margin

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,111 @@
 $(document).ready(function(){
-  $("#development-image").click(function(){
-    $("#development-image").slideDown('2000').hide('2000');
-    $("#development").show('2000');
+  //WHAT WE DO
+  $("div#design-show").click(function(){
+    $(this).slideUp(600);
+    $("div#design-hide").slideDown(600);
   });
-  $("#development").click(function(){
-    $("#development").slideUp('2000');
-    $("#development-image").slideDown('2000');
+
+  $("div#design-hide").click(function(){
+    $(this).slideUp(600);
+    $("div#design-show").slideDown(600);
   });
+
+  $("div#development-show").click(function(){
+    $(this).slideUp(600);
+    $("div#development-hide").slideDown(600);
+  });
+
+  $("div#development-hide").click(function(){
+    $(this).slideUp(600);
+    $("div#development-show").slideDown(600);
+  });
+
+  $("div#product-show").click(function(){
+    $(this).slideUp(600);
+    $("div#product-hide").slideDown(600);
+  });
+
+  $("div#product-hide").click(function(){
+    $(this).slideUp(600);
+    $("div#product-show").slideDown(600);
+  });
+
+  //PROJECTS
+  $("#work1-show").mouseenter(function(){
+    $("#work1-hide").parent().children("#work1-hide").fadeIn();
+  }).mouseleave(function(){
+    $("#work1-hide").parent().children("#work1-hide").fadeOut();
+  });
+
+  $("#work2-show").mouseenter(function(){
+    $("#work2-hide").parent().children("#work2-hide").fadeIn();
+  }).mouseleave(function(){
+    $("#work2-hide").parent().children("#work2-hide").fadeOut();
+  });
+
+  $("#work3-show").mouseenter(function(){
+    $("#work3-hide").parent().children("#work3-hide").fadeIn();
+  }).mouseleave(function(){
+    $("#work3-hide").parent().children("#work3-hide").fadeOut();
+  });
+
+  $("#work4-show").mouseenter(function(){
+    $("#work4-hide").parent().children("#work4-hide").fadeIn();
+  }).mouseleave(function(){
+    $("#work4-hide").parent().children("#work4-hide").fadeOut();
+  });
+
+  $("#work5-show").mouseenter(function(){
+    $("#work5-hide").parent().children("#work5-hide").fadeIn();
+  }).mouseleave(function(){
+    $("#work5-hide").parent().children("#work5-hide").fadeOut();
+  });
+
+  $("#work6-show").mouseenter(function(){
+    $("#work6-hide").parent().children("#work6-hide").fadeIn();
+  }).mouseleave(function(){
+    $("#work6-hide").parent().children("#work6-hide").fadeOut();
+  });
+
+  $("#work7-show").mouseenter(function(){
+    $("#work7-hide").parent().children("#work7-hide").fadeIn();
+  }).mouseleave(function(){
+    $("#work7-hide").parent().children("#work7-hide").fadeOut();
+  });
+
+  $("#work8-show").mouseenter(function(){
+    $("#work8-hide").parent().children("#work8-hide").fadeIn();
+  }).mouseleave(function(){
+    $("#work8-hide").parent().children("#work8-hide").fadeOut();
+  });
+
+  $("#sth").click(function(){
+    $("body").fadeOut(8000);
+    alert("thanos effect");
+  });
+
 });
 
+
+//business logic
 $(document).ready(function(){
-  $("#design-image").click(function(){
-    $("#design-image").slideDown('2000').hide('1000');
-    $("#design").show('2000');
-  });
-  $("#design").click(function(){
-    $("#design").slideUp('2000');
-    $("#design-image").slideDown('2000');
-  });
-});
-
-$(document).ready(function(){
-  $("#product-image").click(function(){
-    $("#product-image").slideDown('2000').hide('1000');
-    $("#product").show('2000');
-  });
-  $("#product").click(function(){
-    $("#product").slideUp('2000');
-    $("#product-image").slideDown('2000');
-  });
-});
-
-$("#work1-show").mouseenter(function(){
-  $("#work1-hide").parent().children("#work1-hide").fadeIn();
-}).mouseleave(function(){
-  $("#work1-hide").parent().children("#work1-hide").fadeOut();
-});
-
-$("#work2-show").mouseenter(function(){
-  $("#work2-hide").parent().children("#work2-hide").fadeIn();
-}).mouseleave(function(){
-  $("#work2-hide").parent().children("#work2-hide").fadeOut();
-});
-$("#work3-show").mouseenter(function(){
-  $("#work3-hide").parent().children("#work3-hide").fadeIn();
-}).mouseleave(function(){
-  $("#work3-hide").parent().children("#work3-hide").fadeOut();
-});
-
-$("#work4-show").mouseenter(function(){
-  $("#work4-hide").parent().children("#work4-hide").fadeIn();
-}).mouseleave(function(){
-  $("#work4-hide").parent().children("#work4-hide").fadeOut();
-});
-
-$("#work5-show").mouseenter(function(){
-  $("#work5-hide").parent().children("#work5-hide").fadeIn();
-}).mouseleave(function(){
-  $("#work5-hide").parent().children("#work5-hide").fadeOut();
-});
-
-$("#work6-show").mouseenter(function(){
-  $("#work6-hide").parent().children("#work6-hide").fadeIn();
-}).mouseleave(function(){
-  $("#work6-hide").parent().children("#work6-hide").fadeOut();
-});
-
-$("#work7-show").mouseenter(function(){
-  $("#work7-hide").parent().children("#work7-hide").fadeIn();
-}).mouseleave(function(){
-  $("#work7-hide").parent().children("#work7-hide").fadeOut();
-});
-
-$("#work8-show").mouseenter(function(){
-  $("#work8-hide").parent().children("#work8-hide").fadeIn();
-}).mouseleave(function(){
-  $("#work8-hide").parent().children("#work8-hide").fadeOut();
-});
-
-
-
-$(document).ready(function(){
-  $("form#form34A").submit(function(event){
-    // event.preventDefault();
-    var name = $("#mc-field-group").val();
-    var email = $("input#MERGE0").val();
-    var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-      alert (name + ", we have received your message. Thank you for reaching out to us.");
-    }
-    else {
-      alert("Please enter your name and email!");
-    }
-    
+  //FORM EVENT//
+  $("#message").keypress(function(){
+    $("#hidden").fadeIn(2000);
   });
 
-});
+  $("#my-form").submit(function(event){
+    let name = $("#name").val();
+    let email = $("#email").val();
+    let message = $("#message").val();
+
+    $("#user").text(name);
+    $(".alert").slideDown();
+    $("#close-alert").click(function(){
+      $(".alert").slideUp();
+      $("#my-form").trigger("reset");
+    });
+    event.preventDefault();
+  });
+
+})
